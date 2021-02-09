95605db20df0a97344d4a5d714c20a674c071454
teefisher2k20 <teefisher314@gmail.com>
2021-02-08 20:24:47 -0600

    bonus mission

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.*;
 public class SkillsController {
 
     @GetMapping()
-    public String hello(){
+    public String hello() {
         return "<html>" +
-                    "<body>" +
-                        "<h1>Skills Tracker</h1>" +
-                         "<h2>We have a few skills we would like to learn. Here is the list: </h2>" +
-                        "<ol>" +
-                            "<li>Java</li>" +
-                            "<li>JavaScript</li>" +
+                "<body>" +
+                "<h1>Skills Tracker</h1>" +
+                "<h2>We have a few skills we would like to learn. Here is the list: </h2>" +
+                "<ol>" +
+                "<li>Java</li>" +
+                "<li>JavaScript</li>" +
                             "<li>Python</li>" +
                         "</ol>" +
                     "</body>" +
@@ -26,45 +26,45 @@ public class SkillsController {
     public String form(){
         String html;
         return "<html>" +
-                    "<body>" +
-                        "<form action='form' method='post'>" +
-                        "<label for='name'>Name: </label><br>" +
-                        "<input id='name' type='text' name='name'/><br>" +
-                        "<label for='favorite-language'>My Favorite Language: </label><br>" +
-                             "<select id='favorite-language' name='language1'>" +
-                            "<option value='Java'>Java</option>" +
-                            "<option value='JavaScript'>JavaScript</option>" +
-                            "<option value='Python'>Python</option>" +
-                        "</select><br>" +
-                            "<label for='second-favorite'>My Second Favorite Language: </label><br>" +
-                            "<select id='second-favorite' name='language2'>" +
+                "<body>" +
+                "<form action='form-submit' method='post'>" +
+                "<label for='name'>Name: </label><br>" +
+                "<input id='name' type='text' name='name'/><br>" +
+                "<label for='favorite-language'>My Favorite Language: </label><br>" +
+                "<select id='favorite-language' name='language1'>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select><br>" +
+                "<label for='second-favorite'>My Second Favorite Language: </label><br>" +
+                "<select id='second-favorite' name='language2'>" +
                             "<option value='Java'>Java</option>" +
                             "<option value='JavaScript'>JavaScript</option>" +
                             "<option value='Python'>Python</option>" +
                             "</select><br>" +
                         "<label for='third-favorite'>My Third Favorite Language: </label><br>" +
                         "<select id='third-favorite' name='language3'>" +
-                        "<option value='Java'>Java</option>" +
-                        "<option value='JavaScript'>JavaScript</option>" +
-                        "<option value='Python'>Python</option>" +
-                        "</select><br>" +
-                        "<input type='submit' value='Submit'/>" +
-                        "</form>" +
-                    "</body>" +
+                "<option value='Java'>Java</option>" +
+                "<option value='JavaScript'>JavaScript</option>" +
+                "<option value='Python'>Python</option>" +
+                "</select><br>" +
+                "<input type='submit' value='Submit'/>" +
+                "</form>" +
+                "</body>" +
                 "</html>";
     }
 
-    //@RequestMapping(value="form-submit", method={RequestMethod.GET, RequestMethod.POST})
-    @PostMapping("form")
-    public String formSubmission(@RequestParam String name, @RequestParam String language1, @RequestParam String language2, @RequestParam String language3 ){
+    @RequestMapping(value = "form-submit", method = {RequestMethod.GET, RequestMethod.POST})
+
+    public String formSubmission(@RequestParam String name, @RequestParam String language1, @RequestParam String language2, @RequestParam String language3) {
         return "<html>" +
                 "<body>" +
                 "<h1>" + name + "</h1>" +
-                "<ol>" +
-                "<li>" + language1 + "</li>" +
-                "<li>" + language2 + "</li>" +
-                "<li>" + language3 + "</li>" +
-                "</ol>" +
+                "<table>" +
+                "<tr>" + language1 + "</tr>" +
+                "<tr>" + language2 + "</tr>" +
+                "<tr>" + language3 + "</tr>" +
+                "</table>" +
                 "</body>" +
                 "</html>";
     }
